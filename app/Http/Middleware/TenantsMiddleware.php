3b228fc4df91052efaa1437233dc945739dc0478
abdullah <abdullah.alhumsi9@gmail.com>
2023-11-14 16:15:58 +0200

    deploy project

---
+++ app/Http/Middleware/TenantsMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Tenant;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+
+class TenantsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $host = $request->getHost();
+        $tenant =  Tenant::where('domain', $host)->firstOrFail();
+        DB::purge('system');
+        Config::set('database.connections.tenant.database', $tenant->database);
+        DB::connection('tenant')->reconnect();
+        DB::setDefaultConnection('tenant');
+        return $next($request);
+    }
+}
