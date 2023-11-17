035e11f38f407d1d3cd1924cb195a19f970cafd8
abdullah <abdullah.alhumsi9@gmail.com>
2023-11-17 23:02:27 +0200

    add some edited

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomePageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
+use App\Http\Controllers\HomePageController;
 
 /*
 |--------------------------------------------------------------------------
